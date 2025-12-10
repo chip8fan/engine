e9710347e47debc5fd8ca3c03ce61310d1e5559a
chip8fan <nguyennamtran2022@gmail.com>
2025-12-10 13:23:22 -0600

    Add files via upload

--- engine.py
+++ engine.py
@@ -1,27 +1,29 @@
-import chess.engine # to load engine
-import chess # to load chess module
-import sys # to get the minimum possible integer value
-import random # to get a random move
-class Engine(): # define a class
-    def play(self, fen, depth, cp_loss, engine_path): # and a function
-        self.board = chess.Board(fen=fen) # set the fen to the input fen
-        self.engine = chess.engine.SimpleEngine.popen_uci(engine_path) # load engine at engine_path
-        self.moves = [] # list of possible moves
-        self.best_moves = [] # list of 'good moves'
-        self.max_score = -sys.maxsize # set max_score to lowest possible integer value
-        for move in self.board.legal_moves: # iterate through all legal moves
-            self.board.push(move) # push move to board
-            if self.board.is_game_over(claim_draw=True) == False: # if game is not over
-                self.score = -self.engine.analyse(self.board, chess.engine.Limit(depth=depth))["score"].pov(self.board.turn).score(mate_score=100000) # analyze a position to the depth specified
-            elif self.board.is_stalemate() or self.board.is_insufficient_material() or self.board.can_claim_draw(): # if the game is a draw
-                self.score = 0 # set the score to 0
-            elif self.board.is_checkmate(): # if checkmate is on the board
-                self.score = 100000 # set the score to 100k
-            self.max_score = max(self.score, self.max_score) # set max_score to the greater of score and max_score
-            self.moves.append([move, self.score]) # append to list of possible moves
-            self.board.pop() # pop move from board
-        self.engine.quit() # quit engine
-        for move in self.moves: # iterate through all legal moves again
-            if move[1]+cp_loss >= self.max_score: # check if move is good enough
-                self.best_moves.append(move[0]) # if so, append to list of 'good moves'
-        return random.choice(self.best_moves) # get a random move
+import chess.engine
+import chess
+import sys
+import random
+class Engine():
+    def play(self, fen, max_time, cp_loss, engine_path):
+        self.board = chess.Board(fen=fen)
+        self.engine = chess.engine.SimpleEngine.popen_uci(engine_path)
+        self.moves = []
+        self.best_moves = []
+        self.max_score = -sys.maxsize
+        max_time /= len(list(self.board.legal_moves))
+        for move in self.board.legal_moves:
+            self.board.push(move)
+            if self.board.is_game_over(claim_draw=True) == False:
+                self.score = -self.engine.analyse(self.board, chess.engine.Limit(time=max_time))["score"].pov(self.board.turn).score(mate_score=sys.maxsize)
+            elif self.board.is_checkmate():
+                self.score = sys.maxsize
+            else:
+                self.score = 0
+            self.max_score = max(self.score, self.max_score)
+            self.moves.append([move, self.score])
+            self.board.pop()
+        self.engine.quit()
+        for move in self.moves:
+            if move[1]+cp_loss >= self.max_score:
+                self.best_moves.append(move[0])
+        print(self.best_moves)
+        return random.choice(self.best_moves)
