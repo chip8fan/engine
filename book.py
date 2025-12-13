bce7df3732567530ccb5721ddaf05a7355a24bd7
Nguyen Tran <me@mac.local>
2025-12-12 21:16:40 -0600

    apply fix

--- book.py
+++ book.py
@@ -2,39 +2,34 @@ import chess
 import chess.engine
 import copy
 import sys
-class bookGenerator():
-    def spawn_engine(self, engine_path):
-        self.engine = chess.engine.SimpleEngine.popen_uci(engine_path)
-    def quit_engine(self):
-        self.engine.quit()
-    def make_pgn(self, move_stack, file_path):
-        self.moves = copy.deepcopy(move_stack)
-        self.chess_board = chess.Board()
-        self.move_count = 1
-        self.pgn_string = ""
-        for move in self.moves:
-            if self.chess_board.turn == chess.WHITE:
-                self.pgn_string = self.pgn_string + f"{self.move_count}. {self.chess_board.san(move)}"
-            elif self.chess_board.turn == chess.BLACK:
-                self.pgn_string = self.pgn_string + f" {self.chess_board.san(move)} "
-                self.move_count += 1
-            self.chess_board.push(move)
-        print(self.pgn_string, file=open(file_path, "a"))
-    def perft(self, depth, move_stack, root_depth, max_advantage, engine_time_limit, file_path):
-        self.nodes = 0
-        if depth == 0:
-            return 1
-        elif depth == root_depth:
-            file = open(file_path, "w")
-            file.close()
-        self.board = chess.Board()
-        for move in move_stack:
-            self.board.push(move)
-        for move in self.board.legal_moves:
-            self.board.push(move)
-            score = abs(self.engine.analyse(self.board, chess.engine.Limit(time=engine_time_limit))["score"].pov(self.board.turn).score(mate_score=sys.maxsize))
-            if score <= max_advantage:
-                self.make_pgn(self.board.move_stack, file_path)
-                self.nodes += self.perft(depth-1, self.board.move_stack, root_depth, max_advantage, engine_time_limit, file_path)
-            self.board.pop()
-        return self.nodes
+def make_pgn(move_stack, file_path):
+    moves = copy.deepcopy(move_stack)
+    chess_board = chess.Board()
+    move_count = 1
+    pgn_string = ""
+    for move in moves:
+        if chess_board.turn == chess.WHITE:
+            pgn_string = pgn_string + f"{move_count}. {chess_board.san(move)}"
+        elif chess_board.turn == chess.BLACK:
+            pgn_string = pgn_string + f" {chess_board.san(move)} "
+            move_count += 1
+        chess_board.push(move)
+    print(pgn_string, file=open(file_path, "a"), end="\n\n")
+def perft(depth, move_stack, root_depth, max_advantage, engine_time_limit, file_path, engine):
+    nodes = 0
+    if depth == 0:
+        return 1
+    elif depth == root_depth:
+        file = open(file_path, "w")
+        file.close()
+    board = chess.Board()
+    for move in move_stack:
+        board.push(move)
+    for move in board.legal_moves:
+        board.push(move)
+        score = abs(engine.analyse(board, chess.engine.Limit(time=engine_time_limit))["score"].pov(board.turn).score(mate_score=sys.maxsize))
+        if score <= max_advantage:
+            make_pgn(board.move_stack, file_path)
+            nodes += perft(depth-1, board.move_stack, root_depth, max_advantage, engine_time_limit, file_path, engine)
+        board.pop()
+    return nodes
--- generator.py
+++ generator.py
@@ -1,16 +1,18 @@
 import book
 import os
 import sys
+import chess.engine
 engine_path = sys.argv[1]
-stockfish = book.bookGenerator()
 initial_depth = 1
 max_eval = 50
 time_limit = 1
 book_path = f"{engine_path.split("/")[-1]}.bin"
 while True:
     file_path = f"{engine_path.split("/")[-1]}-{initial_depth}ply.pgn"
-    stockfish.spawn_engine(engine_path)
-    stockfish.perft(initial_depth, [], initial_depth, max_eval, time_limit, file_path)
-    stockfish.quit_engine()
+    engine = chess.engine.SimpleEngine.popen_uci(engine_path)
+    book.perft(initial_depth, [], initial_depth, max_eval, time_limit, file_path, engine)
+    engine.quit()
     initial_depth += 1
+    if os.path.isfile(book_path):
+        os.remove(book_path)
     os.system(f"jja make --min-games 1 --output {book_path} {file_path}")
